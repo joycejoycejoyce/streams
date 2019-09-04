911322b39703da96a5f6dfc6157ff8a6a0b9ab21
joycejoycejoyce <joycezhou757@gmail.com>
2019-09-04 19:43:36 -0400

    Navigation

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import {BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      app
+      <BrowserRouter> 
+      <Header/>
+      <div>
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/edit" component={StreamEdit} />
+        <Route path="/streams/delete" component={StreamDelete} />
+        <Route path="/streams/show" component={StreamShow} />
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
