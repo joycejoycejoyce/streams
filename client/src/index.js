c85b327d1f7032a584db85000209c38062bc0064
joycejoycejoyce <joycezhou757@gmail.com>
2019-09-27 16:45:15 -0400

    redux dev tool added to chrome

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const composeENhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    reducers,
+    composeENhancers(applyMiddleware())
+    );
 
 ReactDOM.render(
     <Provider store={store}> 
